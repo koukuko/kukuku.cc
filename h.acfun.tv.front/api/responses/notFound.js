279dccd3ff11d16191c0d20c7930966e2db755f4
Akino Mizuho.Koukuko <9@acfun.tv>
2014-09-11 23:48:44 +0800

    [Fix] 生产环境下不忽略提示信息

--- h.acfun.tv.front/api/responses/notFound.js
+++ h.acfun.tv.front/api/responses/notFound.js
@@ -36,9 +36,9 @@ module.exports = function notFound (data, options) {
   // Only include errors in response if application environment
   // is not set to 'production'.  In production, we shouldn't
   // send back any identifying information about errors.
-  if (sails.config.environment === 'production') {
-    data = undefined;
-  }
+//  if (sails.config.environment === 'production') {
+//    data = undefined;
+//  }
 
   // If the user-agent wants JSON, always respond with JSON
   if (req.wantsJSON) {
