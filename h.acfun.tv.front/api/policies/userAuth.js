5ea8ccea55b5deb340a5d2e0e7b5c2c5a3617e47
Akino Mizuho.Koukuko <9@acfun.tv>
2014-10-09 16:46:51 +0800

    [fix] 饼干创建和续费的Invalid Date问题

--- h.acfun.tv.front/api/policies/userAuth.js
+++ h.acfun.tv.front/api/policies/userAuth.js
@@ -5,13 +5,35 @@
  * @description :: 用户有效性检查
  *
  */
+
+var signature = require('cookie-signature');
+
+// 饼干验证
+function parseCookie(str){
+
+    var secret = sails.config.session.secret;
+
+    return str.substr(0, 2) === 's:'
+        ? signature.unsign(str.slice(2), secret)
+        : str;
+}
+
+
+
+// 本篇
 module.exports = function (req, res, next) {
+
+    // 0. 字符串传值
+    if (req.query.userId ){
+
+    }
+
 
     // 1. 是否有饼干
     if (req.signedCookies && req.signedCookies.userId) {
 
         // 饼干续费
-        res.cookie('userId', req.signedCookies.userId, { maxAge: H.settings.cookieExpires, signed: true });
+        res.cookie('userId', req.signedCookies.userId, { maxAge: Number(H.settings.cookieExpires) || 900000, signed: true });
 
     } else if (H.settings.cookieSignup == 'true' || (req.signedCookies && req.signedCookies.managerId)) {
 
@@ -23,7 +45,7 @@ module.exports = function (req, res, next) {
             userId += char.charAt(Math.ceil(Math.random() * seed) % char.length);
         }
 
-        res.cookie('userId', userId, { maxAge: H.settings.cookieExpires, signed: true });
+        res.cookie('userId', userId, { maxAge: Number(H.settings.cookieExpires) || 900000, signed: true });
         req.signedCookies.userId = userId;
 
     } else {
